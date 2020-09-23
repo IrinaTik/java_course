7054ea671819a8d4460cdb234b879a04439320b0
Tikhomirova <i.tikhomirova6@gmail.com>
2020-09-23 14:35:13 +0300

    The 4.2 assignment (min & max values) done

---
+++ 04_NumbersStringsAndDates/TypesBounds/src/Main.java
@@ -0,0 +1,31 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("BYTE");
+        System.out.println("Минимальное значение: " + Byte.MIN_VALUE);
+        System.out.println("Максимальное значение: " + Byte.MAX_VALUE);
+        System.out.println("==================================================");
+        System.out.println("SHORT");
+        System.out.println("Минимальное значение: " + Short.MIN_VALUE);
+        System.out.println("Максимальное значение: " + Short.MAX_VALUE);
+        System.out.println("==================================================");
+        System.out.println("INTEGER");
+        System.out.println("Минимальное значение: " + Integer.MIN_VALUE);
+        System.out.println("Максимальное значение: " + Integer.MAX_VALUE);
+        System.out.println("==================================================");
+        System.out.println("LONG");
+        System.out.println("Минимальное значение: " + Long.MIN_VALUE);
+        System.out.println("Максимальное значение: " + Long.MAX_VALUE);
+
+        System.out.println("==================================================");
+        System.out.println("FLOAT");
+        System.out.println("Минимальное значение: " + Float.MIN_VALUE);
+        System.out.println("Максимальное значение: " + Float.MAX_VALUE);
+        System.out.println("==================================================");
+        System.out.println("DOUBLE");
+        System.out.println("Минимальное значение: " + Double.MIN_VALUE);
+        System.out.println("Максимальное значение: " + Double.MAX_VALUE);
+
+    }
+
+}
