e75f481efe5678f194fa150416f04012cf86086b
Tikhomirova <i.tikhomirova6@gmail.com>
2020-09-23 09:56:07 +0300

    The 4.1 additional assignment (digits sum) done

--- 04_NumbersStringsAndDates/IntegerExperiments/src/Main.java
+++ 04_NumbersStringsAndDates/IntegerExperiments/src/Main.java
@@ -7,6 +7,10 @@ public class Main
         System.out.println("Сумма цифр числа 12345: " + sumDigits(12345));
         System.out.println("Сумма цифр числа 10: " + sumDigits(10));
         System.out.println("Сумма цифр числа 5059191: " + sumDigits(5059191));
+        System.out.println();
+        System.out.println("Сумма цифр числа 12345 (с использованием метода из Character): " + sumDigitsCharacterUse(12345));
+        System.out.println("Сумма цифр числа 10 (с использованием метода из Character): " + sumDigitsCharacterUse(10));
+        System.out.println("Сумма цифр числа 5059191 (с использованием метода из Character): " + sumDigitsCharacterUse(5059191));
     }
 
     public static Integer sumDigits(Integer number)
@@ -15,6 +19,18 @@ public class Main
         int sum = 0;
         for (int i = 0; i < sNumber.length(); i++) {
             sum += Integer.parseInt(String.valueOf(sNumber.charAt(i)));
+        }
+        return sum;
+    }
+
+
+    public static Integer sumDigitsCharacterUse(Integer number)
+    {
+        String sNumber = Integer.toString(number);
+        char[] arrChar = sNumber.toCharArray();
+        int sum = 0;
+        for (int i = 0; i < arrChar.length; i++) {
+            sum += Character.getNumericValue(arrChar[i]);
         }
         return sum;
     }
