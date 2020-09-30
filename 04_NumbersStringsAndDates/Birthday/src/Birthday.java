eeffd9bf59eae971a63664936ddc031c447dd6a1
Tikhomirova <i.tikhomirova6@gmail.com>
2020-09-30 10:26:27 +0300

    The 4.6 assignment edited - the output format was changed

--- 04_NumbersStringsAndDates/Birthday/src/Birthday.java
+++ 04_NumbersStringsAndDates/Birthday/src/Birthday.java
@@ -1,6 +1,7 @@
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Locale;
 
 public class Birthday {
 
@@ -10,15 +11,19 @@ public class Birthday {
         int year = 1989;
 
 
-        DateFormat dateFormat = new SimpleDateFormat("dd MMMM yyyy, EEEE");
+        DateFormat dateFormatRus = new SimpleDateFormat("dd.MM.yyyy - EEEE");
+        DateFormat dateFormatEng = new SimpleDateFormat("dd.MM.yyyy - E", Locale.ENGLISH);
         Calendar calendar = Calendar.getInstance();
         calendar.set(year, month, day);
 
         int currentYear = Calendar.getInstance().get(Calendar.YEAR);
 
+        int i = 0;
         while (calendar.get(Calendar.YEAR) <= currentYear) {
-            System.out.println(dateFormat.format(calendar.getTime()));
+            System.out.println(i + " - " + dateFormatRus.format(calendar.getTime()));
+            System.out.println(i + " - " + dateFormatEng.format(calendar.getTime()));
             calendar.add(Calendar.YEAR, 1);
+            i++;
         }
     }
 
