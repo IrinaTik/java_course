b253cfc5c068ee04f44f46dd39554a5b837968cf
Tikhomirova <i.tikhomirova6@gmail.com>
2020-10-04 17:32:32 +0300

    The 4.6 assignment edited

--- 04_NumbersStringsAndDates/Birthday/src/Birthday.java
+++ 04_NumbersStringsAndDates/Birthday/src/Birthday.java
@@ -1,30 +1,27 @@
 import java.text.DateFormat;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Locale;
+import java.util.Scanner;
 
 public class Birthday {
 
-    public static void main(String[] args) {
-        int day = 19;
-        int month = 0;
-        int year = 1989;
-
-
+    public static void main(String[] args) throws ParseException {
+        Scanner scanner = new Scanner(System.in);
+        DateFormat inputDateFormat = new SimpleDateFormat("dd.MM.yyyy");
         DateFormat dateFormatRus = new SimpleDateFormat("dd.MM.yyyy - EEEE");
         DateFormat dateFormatEng = new SimpleDateFormat("dd.MM.yyyy - E", Locale.ENGLISH);
+
         Calendar calendar = Calendar.getInstance();
-        calendar.set(year, month, day);
+        calendar.setTime(inputDateFormat.parse(scanner.nextLine()));
 
-        int currentYear = Calendar.getInstance().get(Calendar.YEAR);
+        Calendar currentCalendar = Calendar.getInstance();
 
-        int i = 0;
-        while (calendar.get(Calendar.YEAR) <= currentYear) {
+        for (int i = 0; currentCalendar.after(calendar); i++) {
             System.out.println(i + " - " + dateFormatRus.format(calendar.getTime()));
             System.out.println(i + " - " + dateFormatEng.format(calendar.getTime()));
             calendar.add(Calendar.YEAR, 1);
-            i++;
         }
     }
-
 }
