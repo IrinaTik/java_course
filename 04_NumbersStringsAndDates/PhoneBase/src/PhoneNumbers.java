22e1c5abae2585f53797b6562e9c0accef002211
Tikhomirova <i.tikhomirova6@gmail.com>
2020-10-04 15:25:20 +0300

    The 4.5 assignment - additional things

--- 04_NumbersStringsAndDates/PhoneBase/src/PhoneNumbers.java
+++ 04_NumbersStringsAndDates/PhoneBase/src/PhoneNumbers.java
@@ -1,7 +1,45 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class PhoneNumbers {
 
-    public static void main(String[] args) {
-        String phoneNumber = "8-905-12345672342";
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String phoneNumber = reader.readLine();
+
+        String formattedPhone = phoneNumber.replaceAll("[^0-9]", "");
+
+        // формат типа "79993332211"
+        Pattern pattern = Pattern.compile("^(?<head>7)(?<brackets>[0-9]{3,3})(?<three>[0-9]{3,3})(?<twoM>[0-9]{2,2})(?<twoE>[0-9]{2,2})$");
+        while (true) {
+            Matcher matcher = pattern.matcher(formattedPhone);
+            if (matcher.matches()) {
+                System.out.println("+" + matcher.group("head") + " (" + matcher.group("brackets") + ") " + matcher.group("three")
+                        + "-" + matcher.group("twoM") + "-" + matcher.group("twoE"));
+                break;
+            } else {
+                if (formattedPhone.length() == 10) {
+                    formattedPhone = "7" + formattedPhone;
+                } else {
+                    if (formattedPhone.length() == 11) {
+                        if (formattedPhone.charAt(0) == '8') {
+                            formattedPhone = formattedPhone.replaceFirst("8", "7");
+                        }
+                    } else {
+                        System.out.println("Неверный формат номера");
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+
+    private static void oldWays() {
+        String phoneNumber = "8-905-1234567";
 
         String formattedPhone = phoneNumber.replaceAll("[^0-9]", "");
 
