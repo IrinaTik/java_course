54a15b5fcdbe27da3a001e5fef8220874008313c
Tikhomirova <i.tikhomirova6@gmail.com>
2020-10-04 15:25:20 +0300

    The 4.5 assignment - additional things

--- 04_NumbersStringsAndDates/Introductions/src/Intro.java
+++ 04_NumbersStringsAndDates/Introductions/src/Intro.java
@@ -12,17 +12,21 @@ public class Intro {
 
             String[] arrStr = str.split("\\s");
 
-            if (arrStr.length < 3) {
-                if ((arrStr.length == 2) && (!arrStr[1].contains("вна")) && (!arrStr[1].contains("вич"))) {
-                    // вариант для иностранцев, у которых нет отчества
-                    output(arrStr[0], arrStr[1], "<пусто>");
-                    break;
+            if (areAllNamesValid(arrStr)) {
+                if (arrStr.length < 3) {
+                    if ((arrStr.length == 2) && (!arrStr[1].contains("вна")) && (!arrStr[1].contains("вич"))) {
+                        // вариант для иностранцев, у которых нет отчества
+                        output(arrStr[0], arrStr[1], "<пусто>");
+                        break;
+                    } else {
+                        System.out.println("Неверно введено ФИО. Попробуйте еще раз.");
+                    }
                 } else {
-                    System.out.println("Неверно введено ФИО. Попробуйте еще раз.");
+                    output(arrStr[0], arrStr[1], arrStr[2]);
+                    break;
                 }
             } else {
-                output(arrStr[0], arrStr[1], arrStr[2]);
-                break;
+                System.out.println("Неверно введено ФИО. Попробуйте еще раз.");
             }
         }
     }
@@ -31,5 +35,26 @@ public class Intro {
         System.out.println("Фамилия: " + str1);
         System.out.println("Имя: " + str2);
         System.out.println("Отчество: " + str3);
+    }
+
+    private static boolean areAllNamesValid(String[] str) {
+        for (int i = 0; i < str.length; i++) {
+            if (!isNameValid(str[i])) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static boolean isNameValid(String name) {
+        if (!Character.isUpperCase(name.charAt(0))) {
+            return false;
+        }
+        for (int i = 1; i < name.length(); i++) {
+            if (!Character.isLetter(name.charAt(i))) {
+                return false;
+            }
+        }
+        return true;
     }
 }
