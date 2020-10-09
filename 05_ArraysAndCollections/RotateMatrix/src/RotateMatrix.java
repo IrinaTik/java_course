6b730062ce874c37b821ae1ba86fec6cea547f40
Tikhomirova <i.tikhomirova6@gmail.com>
2020-10-09 15:02:23 +0300

    The 5.1 additional assignment (rotating matrix) edited

--- 05_ArraysAndCollections/RotateMatrix/src/RotateMatrix.java
+++ 05_ArraysAndCollections/RotateMatrix/src/RotateMatrix.java
@@ -14,13 +14,16 @@ public class RotateMatrix {
     }
 
     private static int[][] rotate(int[][] matrix){
-        int[][] resMatrix = new int[matrix.length][matrix.length];
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix.length; j++) {
-                resMatrix[j][matrix.length - i - 1] = matrix[i][j];
+        for (int i = 0; i <= matrix.length - 2; i++) {
+            for (int j = i; j <= matrix.length - 2; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[matrix.length - j - 1][i];
+                matrix[matrix.length - j - 1][i] = matrix[matrix.length - i - 1][matrix.length - j - 1];
+                matrix[matrix.length - i - 1][matrix.length - j - 1] = matrix[j][matrix.length - i - 1];
+                matrix[j][matrix.length - i - 1] = temp;
             }
         }
-        return resMatrix;
+        return matrix;
     }
 
     private static int[][] input() {
