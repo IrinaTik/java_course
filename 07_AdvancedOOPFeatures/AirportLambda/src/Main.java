cee676aa694a57cb75b876132703aeab3f7cd13c
Tikhomirova <i.tikhomirova6@gmail.com>
2020-10-31 18:53:24 +0300

    The 7.2.2 assignment (stream airport flights) edited

--- 07_AdvancedOOPFeatures/AirportLambda/src/Main.java
+++ 07_AdvancedOOPFeatures/AirportLambda/src/Main.java
@@ -1,9 +1,7 @@
 import com.skillbox.airport.Airport;
 import com.skillbox.airport.Flight;
 
-import java.util.ArrayList;
 import java.util.Calendar;
-import java.util.List;
 
 public class Main {
 
@@ -11,17 +9,16 @@ public class Main {
         Airport airport = Airport.getInstance();
         System.out.println("Количество самолетов в аэропорту: " + airport.getAllAircrafts().size());
 
-        // формирование списка всех поетов
-        List<Flight> flights = new ArrayList<>();
-        airport.getTerminals().forEach(t -> flights.addAll(t.getFlights()));
-
-        // определение временного интервала
         Calendar now = Calendar.getInstance();
         Calendar in2hours = Calendar.getInstance();
         in2hours.add(Calendar.HOUR, 2);
 
         System.out.println("=====In 2 hours=====");
-        flights.stream().filter(f -> f.getDate().before(in2hours.getTime()) && f.getDate().after(now.getTime())).forEach(System.out::println);
+        airport.getTerminals().stream()
+                .flatMap(t -> t.getFlights().stream())
+                .filter(f -> f.getDate().before(in2hours.getTime()) && f.getDate().after(now.getTime()))
+                .filter(f -> f.getType().equals(Flight.Type.DEPARTURE))
+                .forEach(System.out::println);
 
     }
 
