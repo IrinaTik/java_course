eb7f72ec66a3a84d12f89cce56633fe27c0ed43c
Tikhomirova <i.tikhomirova6@gmail.com>
2020-12-02 16:07:28 +0300

    The 9.5 assignment (Moscow metro: parse html files, read\write json files) in progress - writing to JSON file

--- 09_FilesAndNetwork/MoscowMetro/src/main/java/MosMetro.java
+++ 09_FilesAndNetwork/MoscowMetro/src/main/java/MosMetro.java
@@ -1,11 +1,16 @@
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import java.io.FileWriter;
+
 public class MosMetro {
 
     private static final String SITE_URL = "https://www.moscowmap.ru/metro.html#lines";
+    private static final String JSONFILE_PATH = "./09_FilesAndNetwork/MoscowMetro/src/main/resources/MoscowMetroMap.json";
 
     public static void main(String[] args) {
         parseHTML();
@@ -20,10 +25,19 @@ public class MosMetro {
             Elements linesInfo = doc.select("div[class*=\"js-toggle-depend\"]");
             Elements stationsInfo  = doc.select("span[class=\"name\"]");
             Elements stationsInfoLine1 = doc.select("div[data-line=1]").select("span[class=name]");
+
+            JSONObject metro = new JSONObject();
+            JSONArray linesArray = new JSONArray();
             linesInfo.forEach(line -> {
                 System.out.println(line.text());
                 System.out.println(line.select("span[data-line]").attr("data-line"));
+                JSONObject jLine = new JSONObject();
+                jLine.put("name", line.text());
+                jLine.put("number", line.select("span[data-line]").attr("data-line"));
+                linesArray.add(jLine);
             });
+            metro.put("lines", linesArray);
+
             System.out.println("=====================");
             Line line1 = new Line(linesInfo.get(0).text(),
                     linesInfo.get(0).select("span[data-line]").attr("data-line"));
@@ -33,9 +47,18 @@ public class MosMetro {
             });
             System.out.println("=====================");
             System.out.println(line1.toString());
+
+            FileWriter file = new FileWriter(JSONFILE_PATH);
+            file.write(metro.toJSONString());
+            file.flush();
+            file.close();
         } catch (Exception ex) {
             ex.printStackTrace();
         }
+
+    }
+
+    private static void jsonWrite() {
 
     }
 
