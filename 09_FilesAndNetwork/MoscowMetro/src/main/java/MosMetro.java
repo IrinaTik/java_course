e4bf33ce9863a684a180062bacdc930de0c03526
Tikhomirova <i.tikhomirova6@gmail.com>
2020-12-02 21:06:54 +0300

    The 9.5 assignment (Moscow metro: parse html files, read\write json files) in progress - adding stations and connections without writing in file

--- 09_FilesAndNetwork/MoscowMetro/src/main/java/MosMetro.java
+++ 09_FilesAndNetwork/MoscowMetro/src/main/java/MosMetro.java
@@ -6,28 +6,37 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.FileWriter;
+import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class MosMetro {
 
     private static final String SITE_URL = "https://www.moscowmap.ru/metro.html#lines";
     private static final String JSONFILE_PATH = "./09_FilesAndNetwork/MoscowMetro/src/main/resources/MoscowMetroMap.json";
 
-    public static void main(String[] args) {
-        parseHTML();
+    public static void main(String[] args) throws IOException {
+         Document doc = Jsoup.connect(SITE_URL).maxBodySize(0).get();
+//         parseHTML(doc);
+        fillConnections(doc);
+
 
 
     }
 
-    private static void parseHTML() {
+    private static void parseHTML(Document doc) {
         try {
-            Document doc = Jsoup.connect(SITE_URL).maxBodySize(0).get();
+            doc = Jsoup.connect(SITE_URL).maxBodySize(0).get();
             Elements metroInfo = doc.select("div[id=\"metrodata\"]");
             Elements linesInfo = doc.select("div[class*=\"js-toggle-depend\"]");
             Elements stationsInfo  = doc.select("span[class=\"name\"]");
             Elements stationsInfoLine1 = doc.select("div[data-line=1]").select("span[class=name]");
 
-            JSONObject metro = new JSONObject();
-            JSONArray linesArray = new JSONArray();
+            metroInfo.forEach(System.out::println);
+
+            JSONObject metro = new JSONObject(); //общий объект
+
+            JSONArray linesArray = new JSONArray(); //массив с линиями
             linesInfo.forEach(line -> {
                 System.out.println(line.text());
                 System.out.println(line.select("span[data-line]").attr("data-line"));
@@ -38,16 +47,22 @@ public class MosMetro {
             });
             metro.put("lines", linesArray);
 
+            //заполнение линии станциями
             System.out.println("=====================");
-            Line line1 = new Line(linesInfo.get(0).text(),
-                    linesInfo.get(0).select("span[data-line]").attr("data-line"));
-            stationsInfoLine1.forEach(element -> {
-                System.out.println(element.text());
-                line1.addStation(element.text());
-            });
-            System.out.println("=====================");
-            System.out.println(line1.toString());
+            JSONObject jAllStations = new JSONObject();
+            for (int i = 0; i < linesArray.size(); i++) {
+                JSONObject l = (JSONObject) linesArray.get(i);
+                String name = (String) l.get("name");
+                String number = (String) l.get("number");
+                Elements stationsCurLine = doc.select("div[data-line=" + number + "]").select("span[class=name]");
+                JSONArray jStations = new JSONArray();
+                stationsCurLine.forEach(station -> jStations.add(station.text()));
+                jAllStations.put(name, jStations);
+            }
+            metro.put("stations", jAllStations);
 
+
+            //запись в JSON файл
             FileWriter file = new FileWriter(JSONFILE_PATH);
             file.write(metro.toJSONString());
             file.flush();
@@ -55,6 +70,45 @@ public class MosMetro {
         } catch (Exception ex) {
             ex.printStackTrace();
         }
+    }
+
+    private static void fillConnections(Document doc) {
+        Elements cons = doc.select("a[data-metrost]");
+
+        cons.forEach(System.out::println);
+        System.out.println("=================");
+        for (Element con : cons) {
+            Elements metroLink = con.select("span[title*=переход на]");
+            if (!metroLink.isEmpty()) {
+                metroLink.forEach(link -> {
+                    String lineName = con.select("span.name").text();
+                    String conStation = getConStationName(link.attr("title"));
+                    String conLine = getConLineNumber(link.attr("class"));
+                    System.out.println(lineName + " - " + conStation + " : " + conLine);
+                });
+            }
+        }
+    }
+
+    private static String getConLineNumber(String number) {
+        Pattern pattern = Pattern.compile("(t-icon-metroln ln-)(.+)");
+        return getConInfo(number, pattern);
+    }
+
+    private static String getConStationName(String name) {
+        Pattern pattern = Pattern.compile("(переход на станцию )«(.+)»(.+)");
+        return getConInfo(name, pattern);
+    }
+
+    private static String getConInfo(String s, Pattern pattern) {
+        Matcher matcher = pattern.matcher(s);
+        if (matcher.find()) {
+            return matcher.group(2).trim();
+        }
+        return "";
+    }
+
+    private static void fillTheLine() {
 
     }
 
