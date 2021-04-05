f97a71961900485536bdf19fd99dd96121ed797d
Tikhomirova <i.tikhomirova6@gmail.com>
2021-04-05 12:08:47 +0300

    The 12.2 assignment (todolist with storage) in progress - PUT and DELETE actions added

--- 12_SpringBootWebDevelopment/ToDoList/src/main/java/BDEmulator/Storage.java
+++ 12_SpringBootWebDevelopment/ToDoList/src/main/java/BDEmulator/Storage.java
@@ -2,18 +2,22 @@ package BDEmulator;
 
 import response.Task;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Storage {
 
     private static int currentId = 1;
     private static Map<Integer, Task> tasks = new HashMap<>();
+    private static Set<Integer> freeIds = new HashSet<>(); // идентификаторы, освободившиеся в результате удаления элементов
 
     public static int addTask(Task task) {
-        int id = currentId++;
+        int id;
+        if (freeIds.isEmpty()) {
+            id = currentId++;
+        } else {
+            id = freeIds.stream().findFirst().get();
+            freeIds.remove(id);
+        }
         task.setId(id);
         tasks.put(id, task);
         return id;
@@ -27,8 +31,28 @@ public class Storage {
 
     public static Task getTask(int id) {
         if (tasks.containsKey(id)) {
-            tasks.get(id);
+            return tasks.get(id);
         }
         return null;
+    }
+
+    public static Task updateTask(int id, Task taskDetails) {
+        Task task = getTask(id);
+        if (task != null) {
+            task.setContext(taskDetails.getContext());
+            task.setWorkerId(taskDetails.getWorkerId());
+            return task;
+        }
+        return null;
+    }
+
+    public static boolean deleteTask(int id) {
+        Task task = getTask(id);
+        if (task != null) {
+            tasks.remove(id);
+            freeIds.add(id);
+            return true;
+        }
+        return false;
     }
 }
--- 12_SpringBootWebDevelopment/ToDoList/src/main/java/main/TaskController.java
+++ 12_SpringBootWebDevelopment/ToDoList/src/main/java/main/TaskController.java
@@ -3,10 +3,7 @@ package main;
 import BDEmulator.Storage;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import response.Task;
 
 import java.util.List;
@@ -19,11 +16,6 @@ public class TaskController {
         return Storage.getAllTasks();
     }
 
-    @PostMapping("/tasks/")
-    public int add(Task task) {
-        return Storage.addTask(task);
-    }
-
     @GetMapping("/tasks/{id}")
     public ResponseEntity<Task> get(@PathVariable int id) {
         Task task =  Storage.getTask(id);
@@ -31,6 +23,29 @@ public class TaskController {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
         }
         return new ResponseEntity<>(task, HttpStatus.OK);
+    }
+
+    @PostMapping("/tasks/")
+    public int add(Task task) {
+        return Storage.addTask(task);
+    }
+
+    @PutMapping("/tasks/{id}")
+    public ResponseEntity<Task> update(@PathVariable int id, @RequestBody Task taskDetails) {
+        Task task = Storage.updateTask(id, taskDetails);
+        if (task == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
+        return new ResponseEntity<>(task, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/tasks/{id}")
+    public ResponseEntity<Integer> delete(@PathVariable int id) {
+        boolean isRemoved = Storage.deleteTask(id);
+        if (!isRemoved) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
+        return new ResponseEntity<>(id, HttpStatus.OK);
     }
 
 }
