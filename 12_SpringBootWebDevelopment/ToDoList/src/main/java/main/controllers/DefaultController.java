461ab1b9b1fd64645be2fcba9e8d8559f4bee026
tikhomirova <i.tikhomirova6@gmail.com>
2021-05-08 19:10:06 +0300

    The 14.4 (former 12.4) assignment (todolist with html view) done

--- 12_SpringBootWebDevelopment/ToDoList/src/main/java/main/controllers/DefaultController.java
+++ 12_SpringBootWebDevelopment/ToDoList/src/main/java/main/controllers/DefaultController.java
@@ -1,15 +1,30 @@
 package main.controllers;
 
+import main.response.Task;
+import main.response.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Date;
+import java.util.ArrayList;
+import java.util.List;
 
-@RestController
+@Controller
 public class DefaultController {
 
+    @Autowired
+    TaskRepository taskRepository;
+
     @RequestMapping("/")
-    public String index() {
-        return "Hello! Current date is " + (new Date().toString());
+    public String index(Model model) {
+        Iterable<Task> taskIterable = taskRepository.findAll();
+        List<Task> tasks = new ArrayList<>();
+        for (Task task : taskIterable) {
+            tasks.add(task);
+        }
+        model.addAttribute("tasks", tasks);
+
+        return "index";
     }
 }
